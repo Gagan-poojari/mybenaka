729b5ba8214f49754832311c9e771c2faa7b5b54
Gagan Poojari <gagancgp@gmail.com>
2025-10-18 19:11:51 +0530

    added: /admin folder in frontend... 75% admin pages done

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthContext from "./contexts/AuthContext";
 import UserContext from "./contexts/UserContext";
+import ClientProviders from "./ClientProviders";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,11 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthContext>
-          <UserContext>
-            {children}
-          </UserContext>
-        </AuthContext>
+        <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
   );
