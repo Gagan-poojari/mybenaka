537f53c3880d5a68ff80f0db3bfc88a0a091c706
Gagan Poojari <gagancgp@gmail.com>
2025-10-26 14:33:52 +0530

    manager role added, admin bugs fied, new features added

--- frontend/app/ClientProviders.jsx
+++ frontend/app/ClientProviders.jsx
@@ -1,11 +1,14 @@
 "use client";
 
 import AuthContext from "./contexts/AuthContext";
+import UserContext from "./contexts/UserContext";
 
 export default function ClientProviders({ children }) {
   return (
     <AuthContext>
+      <UserContext>
         {children}
+      </UserContext>
     </AuthContext>
   );
 }
