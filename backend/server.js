da9c6b2c2ce4d75e291950b7ff2221fa99512bf0
Gagan Poojari <gagancgp@gmail.com>
2025-10-26 14:28:02 +0530

    manager role added, admin bugs fied, new features added

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3001" || "http://localhost:3000",
-  // origin: "https://mybenaka-frontend.onrender.com",
+  // origin: "http://localhost:3001" || "http://localhost:3000",
+  origin: "https://mybenaka-frontend.onrender.com",
   credentials: true
 }));
 
